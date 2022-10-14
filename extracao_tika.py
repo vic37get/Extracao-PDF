fbf9169996fb89c6d79b99974f656ad6ca8b554c
Victor Ribeiro da Silva <victorribeiro172@gmail.com>
2022-10-14 12:45:36 -0300

    extrações

--- extracao_tika.py
+++ extracao_tika.py
@@ -3,6 +3,7 @@ from tika import parser
 import os
 os.environ['TIKA_SERVER_JAR'] = 'https://repo1.maven.org/maven2/org/apache/tika/tika-server/1.19/tika-server-1.19.jar'
 FileName = "_EDITAL_TP02.pdf"
-PDF_Parse = parser.from_file(FileName)
-print(PDF_Parse)
-#print(PDF_Parse)
+PDF_Parse = parser.from_file(FileName, xmlContent=True)
+print(PDF_Parse['content'])
+with open('dados.txt', 'w', encoding='utf-8') as f:
+    f.write(str(PDF_Parse['content']))
