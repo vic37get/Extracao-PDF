9a4f1bbf0a8e21f815343aa0a7a30b92f8b972f4
Victor Ribeiro da Silva <victorribeiro172@gmail.com>
2022-10-21 09:07:02 -0300

    continuação

--- conversions/convertDocAndDocx.py
+++ conversions/convertDocAndDocx.py
@@ -6,13 +6,13 @@ def docAndDocxToPdf(filename, DIST_DIR):
     os.chdir(DIST_DIR)
     subprocess.call(['soffice', '--headless', '--convert-to', 'pdf', filename], stdout=subprocess.DEVNULL)
     filenamePDF = filename.split('.')[0]+'.pdf'
-    if searchPdf(filenamePDF, DIST_DIR) == True:
+    if searchPdf(filenamePDF) == True:
         return os.path.join(DIST_DIR, filenamePDF)
     else:
         return False
 
-def searchPdf(arquivo, DIR):
-    if arquivo in os.listdir(DIR):
+def searchPdf(arquivo):
+    if arquivo in os.listdir('.'):
         return True
     else:
         return False
--- extracaoParsr.py
+++ extracaoParsr.py
@@ -7,12 +7,13 @@ import re
 import os
 from pathlib import Path
 from tqdm import tqdm
-from manipulations.manipulationMarkdown import markDownToText, readMarkDownFile
 from conversions.convertDocAndDocx import docAndDocxToPdf
+import shutil
 
 BASE_DIR = '/mnt/c/Users/victor.silva/Documents/Repositórios/Extracao-PDF'
 DIR_ARQUIVOS = 'arquivos/arquivosPDF'
 DIR_DESTINO = 'arquivos/arquivosTXT'
+OUT_DIR = '/var/projetos/arquivos'
 INPUT_DATAFRAME = readCsv('lic_2007_2022.csv')
 
 def getExtension(tipo):
@@ -20,15 +21,19 @@ def getExtension(tipo):
         if re.search(ext[1],tipo):
             return ext[0]
 
-def downloadPDF(id_licitacao, id_arquivo):
+def downloadPDF(id_licitacao, id_arquivo, OUT_DIR):
     URL = 'http://sistemas.tce.pi.gov.br/muralic/api/licitacoes/{}/arquivos/{}'.format(id_licitacao, id_arquivo)
     try:
-        arquivoPDF = wget.download(URL, "{}/{}-{}".format(DIR_ARQUIVOS, id_licitacao, id_arquivo))
-        tipo = magic.from_file(arquivoPDF)
-        os.rename(arquivoPDF, arquivoPDF+getExtension(tipo))
-        arquivoPDF = arquivoPDF+getExtension(tipo)
+        file = wget.download(URL, "{}/{}-{}".format(DIR_ARQUIVOS, id_licitacao, id_arquivo))
+        tipo = magic.from_file(file)
+        os.rename(file, file+getExtension(tipo))
+        file = file+getExtension(tipo)
+        NEW_DIR = Path(OUT_DIR).joinpath('arquivos_'+getExtension(tipo))
+        filename = Path(file).name
+        NEW_DIR = NEW_DIR.joinpath(filename)
+        shutil.move(file, NEW_DIR)
         #print('\nDownload da licitação {}, arquivo {} realizado!'.format(id_licitacao, id_arquivo))
-        return arquivoPDF
+        return file
     except:
         print('\nErro ao realizar o download da licitação {}, arquivo {}!'.format(id_licitacao, id_arquivo))
         return None
@@ -45,30 +50,34 @@ def PDFtoText(arquivoPDF, id_licitacao, id_arquivo):
     return parsr
 
 def ExtractText(INPUT_DATAFRAME):
-    FAILED_DOWNLOAD, FAILED_CONVERSION = [], []
+    FAILED_DOWNLOAD, FAILED_CONVERSION, DOC = [],[],[]
     progress = tqdm(total=len(INPUT_DATAFRAME))
     for file in INPUT_DATAFRAME.index:
-        '''if file == 20:
-            break'''
+        if file == 100:
+            break
         id_arquivo = getIdArquivo(INPUT_DATAFRAME, file)
         id_licitacao = getIdLicitacao(INPUT_DATAFRAME, file)
-        with suppress_stdout():
-            file_pdf = downloadPDF(id_licitacao, id_arquivo)
+        #with suppress_stdout():
+            #file_pdf = downloadPDF(id_licitacao, id_arquivo)
+        file_pdf = downloadPDF(id_licitacao, id_arquivo)
         if file_pdf != None:
-            pathPdfFile = Path(file_pdf)
+            PDFtoText(file_pdf, id_licitacao, id_arquivo)
+            '''pathPdfFile = Path(file_pdf)
             if pathPdfFile.suffix.find('.doc')!=-1:
-                file_pdf = docAndDocxToPdf(pathPdfFile.name, DIR_ARQUIVOS)
-                os.remove(pathPdfFile.name)
-                os.chdir(BASE_DIR)
+                DOC.append('{}-{}\n'.format(id_licitacao, id_arquivo))
+                #file_pdf = docAndDocxToPdf(pathPdfFile.name, DIR_ARQUIVOS)
+                #os.remove(pathPdfFile.name)
+                #os.chdir(BASE_DIR)
             if file_pdf != False:
                 PDFtoText(file_pdf, id_licitacao, id_arquivo)
             else:
-                FAILED_CONVERSION.append('{}-{}\n'.format(id_licitacao, id_arquivo))
+                FAILED_CONVERSION.append('{}-{}\n'.format(id_licitacao, id_arquivo))'''
         else:
             FAILED_DOWNLOAD.append('{}-{}\n'.format(id_licitacao, id_arquivo))
         progress.update(1)
     saveFile(FAILED_DOWNLOAD, 'failedDownload.txt')
     saveFile(FAILED_CONVERSION, 'failedConversion.txt')
+    saveFile(FAILED_CONVERSION, 'docAndDocxFiles.txt')
     removeArquivosPDF(DIR_ARQUIVOS)
 
 if __name__ == "__main__":
