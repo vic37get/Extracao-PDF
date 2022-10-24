0abac00dbb3c288f28c0b3310fd18dc8332e25cb
Victor Ribeiro da Silva <victorribeiro172@gmail.com>
2022-10-24 11:03:45 -0300

    extractTextFromDir

--- manipulations/manipulationMarkdown.py
+++ manipulations/manipulationMarkdown.py
@@ -1,6 +1,17 @@
 import repackage
 repackage.up()
 from utils.regularExpressions import *
+from bs4 import BeautifulSoup
+from markdown import markdown
+
+def markDownToTextBs(text_markdown):
+    text_markdown = markdown(text_markdown)
+    text_markdown = ''.join(BeautifulSoup(text_markdown, features="html.parser").findAll(text=True))
+    text_markdown = removeTables(text_markdown)
+    text_markdown = removeBreakPage(text_markdown)
+    text_markdown = removeAsterisk(text_markdown)
+
+    return text_markdown
 
 def markDownToText(text_markdown):
     text_markdown = removeTables(text_markdown)
@@ -17,9 +28,18 @@ def saveMarkdownToText(dataText, fileName):
     with open(fileName, 'w') as file:
         file.write(dataText)
 
-#-----------------
-mkfile = readMarkDownFile('manipulations/sampleFile.md')
-data = markDownToText(mkfile)
-saveMarkdownToText(data, 'exemplomd.txt')
+def readMarkDownFileMK(fileMarkDown):
+    with open(fileMarkDown, 'r') as file:
+        release_note = file.read()
+        description = bytes(release_note, 'utf-8')
+    return description.decode("utf-8")
 
+#-----------------
+
+'''mkfile = readMarkDownFile('manipulations/100024-49203.md')
+data = markDownToText(mkfile)
+saveMarkdownToText(data, 'exemplomd.txt')'''
+'''mk = readMarkDownFileMK('manipulations/samplefile.md')
+mk = markDownToTextBs(mk)
+saveMarkdownToText(mk, 'exemplomd.txt')'''
 
