8bfbc95810eb4b00a2f4447cbcebc765eae1e9cf
Victor Ribeiro da Silva <victorribeiro172@gmail.com>
2022-10-26 09:15:04 -0300

    deleções de arquivos desnecessários

--- convertDocAndDocx.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-def docAndDocxToPdf(filename, DIST_DIR):
-    os.chdir(DIST_DIR)
-    subprocess.call(['soffice', '--headless', '--convert-to', 'pdf', filename],stdout=subprocess.DEVNULL)
-    filenamePDF = filename.split('.')[0]+'.pdf'
-    return os.path.join(DIST_DIR, filenamePDF)
-
